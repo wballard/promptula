012f3997527c6eb30c6dfcbaf3b82c530b8081f3
Will Ballard <wballard@mailframe.net>
2012-10-29 16:30:46 -0500

    clean

--- lib/promptula.rb
+++ lib/promptula.rb
@@ -3,22 +3,44 @@ require "rainbow"
 module Promptula
   SEPARATOR = "\u25B6"
   SEPARATOR_THIN = "\u276F"
+  PATH_SEPARATOR = "\u22F0"
+  PATH_SEPARATOR = '/'
   BACKGROUND = "3a3a3a"
+
+  def self.pre(color)
+    SEPARATOR.background(color).foreground(BACKGROUND)
+  end
+
+  def self.post(color)
+    SEPARATOR.foreground(color)
+  end
 
   def self.cwd()
     current = Dir.pwd
     home = ENV['HOME']
     ret = ''
     current.sub(home, '~').split('/').each do |segment|
-      ret += " #{segment} ".foreground(:default).background BACKGROUND
-      ret += SEPARATOR_THIN.foreground("808080").background BACKGROUND
+      ret += PATH_SEPARATOR.foreground("808080").background BACKGROUND if segment != '~'
+      ret += "#{segment}".foreground(:default).background BACKGROUND
     end
-    ret
+    ret + ' '.background(BACKGROUND)
   end
 
   def self.git()
-    branch = `git branch`.sub('*', '').chomp
-    branch
+    branch = '<unknown>'
+    `git branch`.split("\n").each do |line|
+      puts line
+      if line[0] == '*'
+        branch = line.sub('*', '').chomp
+      end
+    end
+    status = `git status --porcelain`
+    untracked = status.match('\?\?').size > 0 ? '+' : ''
+    dirty = status.size > 0
+    background = dirty ? :red : :green
+    pre(background) +
+    "#{branch}#{untracked} ".background(dirty ? :red : :green).bright +
+    post(background)
   end
 
   def self.prompt()
